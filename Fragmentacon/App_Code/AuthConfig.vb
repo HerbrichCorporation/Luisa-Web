1cdca5ff21553175e98fddfcf9a7afddd43064f1
Jennifer Herbrich <xx_jenni_xx_@hotmail.de>
2020-05-15 00:09:21 +0200

    Projektdateien hinzufügen.

---
+++ Fragmentacon/App_Code/AuthConfig.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.AspNet.Membership.OpenAuth
+
+Public Module AuthConfig
+    Sub RegisterOpenAuth()
+        ' Einzelheiten zur Einrichtung von ASP.NET finden Sie unter https://go.microsoft.com/fwlink/?LinkId=252803
+        '-Anwendung für die Unterstützung der Anmeldung über externe Dienste zu erhalten.
+
+        'OpenAuth.AuthenticationClients.AddTwitter(
+        '    consumerKey:= "Ihr Twitter-Consumerschlüssel",
+        '    consumerSecret:= "Ihr geheimer Twitter-Consumerschlüssel")
+
+        'OpenAuth.AuthenticationClients.AddFacebook(
+        '    appId:= "Ihre Facebook-Anwendungs-ID",
+        '    appSecret:= "Ihr geheimer Facebook-Anwendungsschlüssel")
+
+        'OpenAuth.AuthenticationClients.AddMicrosoft(
+        '    clientId:= "Client-ID Ihres Microsoft-Kontos",
+        '    clientSecret:= "Geheimer Schlüssel Ihres Microsoft-Kontos")
+
+        'OpenAuth.AuthenticationClients.AddGoogle()
+    End Sub
+End Module
+
